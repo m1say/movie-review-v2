a07d50cd98ac4810db502eff11b0ddab3c00987d
Mark Cobo <mark@Marks-MBP.bbrouter>
2023-08-19 12:08:52 +0800

    Update templates

---
+++ main/apps/movie_review/templatetags/custom_filters.py
@@ -0,0 +1,21 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.timesince import timesince
+
+register = template.Library()
+
+
+@register.filter(name="biggest_timesince")
+def biggest_timesince_filter(value, arg=None):
+    """Format a date as the time since that date but only returns the first time unit."""
+    ret = ""
+    if not value:
+        return ret
+    try:
+        if arg:
+            ret = timesince(value, arg, depth=1)
+        else:
+            ret = timesince(value, depth=1)
+    except (ValueError, TypeError):
+        return ""
+    return ret
